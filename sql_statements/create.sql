647fd12e622bcee75cfec08a35e9d7d1a5a50fd4
Carter <44276294+CarterM2607@users.noreply.github.com>
2022-12-01 18:40:03 -0700

    Changes to field lengths and names
    Replaced any fields ending with underscore with a more suitable name. Email field was not remotely long enough to store most email addresses. The standard length of an email address is defined as 320 characters
    
    Changed Credit Card Number to CHAR(16) instead of INT: 10^16 > 2^31, so card numbers would have caused overflow.
    Split name field into first and last name which is standard.
    
    Note that the specialization classes of User are made redundant by the UserType attribute. It would make more sense to have StAddress and CreditCard as Null-able fields, only filled when a user becomes a registered user. This was left untouched.

--- sql_statements/create.sql
+++ sql_statements/create.sql
@@ -1,101 +1,82 @@
 DROP DATABASE IF EXISTS `Theater`;
 CREATE DATABASE `Theater`;
 USE Theater;
-
-CREATE TABLE User_ 
-(
-    Email   VARCHAR(32) NOT NULL,
-    Name_   VARCHAR(30) NOT NULL,
+CREATE TABLE Base_User(
+    Email   VARCHAR(320) NOT NULL,
+    Fname  VARCHAR(30) NOT NULL,
+    Lname  VARCHAR(30) NOT NULL,
     Password_ VARCHAR(32) NOT NULL,
     UserType VARCHAR(10) NOT NULL,
     PRIMARY KEY(Email)
 );
-
-CREATE TABLE Registered 
-(
-    Email   VARCHAR(32) NOT NULL,
-    Address_ VARCHAR(250) NOT NULL,
-    CreditCard INT NOT NULL,
+CREATE TABLE Registered_User(
+    Email   VARCHAR(320) NOT NULL,
+    StAddress VARCHAR(400) NOT NULL,
+    CreditCard CHAR(16) NOT NULL,
     PRIMARY KEY(Email),
-    FOREIGN KEY(Email) REFERENCES User_(Email)
+    FOREIGN KEY(Email) REFERENCES Base_User(Email)
     ON UPDATE CASCADE ON DELETE CASCADE
 );
-
-CREATE TABLE Guest 
-(
-    Email   VARCHAR(32) NOT NULL,
+CREATE TABLE Guest_User(
+    Email   VARCHAR(320) NOT NULL,
     PRIMARY KEY(Email),
-    FOREIGN KEY(Email) REFERENCES User_(Email)
+    FOREIGN KEY(Email) REFERENCES Base_User(Email)
     ON UPDATE CASCADE ON DELETE CASCADE
 );
-
-CREATE TABLE Admin 
-(
-    Email   VARCHAR(32) NOT NULL,
+CREATE TABLE Admin_User(
+    Email   VARCHAR(320) NOT NULL,
     PRIMARY KEY(Email),
-    FOREIGN KEY(Email) REFERENCES User_(Email)
+    FOREIGN KEY(Email) REFERENCES Base_User(Email)
     ON UPDATE CASCADE ON DELETE CASCADE
 );
-
-CREATE TABLE Credit
-(
+CREATE TABLE Credit(
     ID INT AUTO_INCREMENT NOT NULL,
-    Email VARCHAR(32) NOT NULL,
-    ExpirationDate DATE NOT NULL,
+    Email VARCHAR(320) NOT NULL,
+    ExpiryDate DATE NOT NULL,
     Amount INT NOT NULL,
     PRIMARY KEY(ID, Email),
-    FOREIGN KEY(Email) REFERENCES User_(Email)
+    FOREIGN KEY(Email) REFERENCES Base_User(Email)
     ON UPDATE CASCADE ON DELETE CASCADE
 );
-
-CREATE TABlE MovieTheater
-(
-    Name_ VARCHAR(32) NOT NULL,
-    Address_ VARCHAR(250) NOT NULL,
-    PRIMARY KEY(Name_)
+CREATE TABlE MovieTheater(
+    LocName   VARCHAR(32) NOT NULL,
+    StAddress VARCHAR(400) NOT NULL,
+    PRIMARY KEY(LocName)
 );
-
-CREATE TABLE TheaterRoom
-(
+CREATE TABLE TheaterRoom(
     RNumber INT NOT NULL,
     Capacity INT NOT NULL,
     TheaterName VARCHAR(32) NOT NULL,
     PRIMARY KEY(RNumber, TheaterName),
-    FOREIGN KEY(TheaterName) REFERENCES MovieTheater(Name_)
+    FOREIGN KEY(TheaterName) REFERENCES MovieTheater(LocName)
     ON UPDATE CASCADE ON DELETE CASCADE
 );
-
-CREATE TABlE Movie
-(
+CREATE TABlE Movie(
     Title VARCHAR(32) NOT NULL,
-    Duration INT NOT NULl,
+    Duration INT NOT NULL,
     Rating VARCHAR(5) NOT NULL,
     PRIMARY KEY(Title)
 );
-
-CREATE TABLE Showtime 
-(
+CREATE TABLE Showtime(
+    ShowDateTime DATETIME NOT NULL,
     MTitle VARCHAR(32) NOT NULL,
     RNumber INT NOT NULL,
     TName VARCHAR(32) NOT NULL,
-    DateTime_ DATETIME NOT NULL,
-    PRIMARY KEY(DateTime_, MTitle, RNumber, TName),
+    PRIMARY KEY(ShowDateTime, MTitle, RNumber, TName),
     FOREIGN KEY(MTitle) REFERENCES Movie(Title)
     ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY(RNumber, TName) REFERENCES TheaterRoom(RNumber, TheaterName)
     ON UPDATE CASCADE ON DELETE CASCADE
 );
-
-CREATE TABLE Ticket 
-(
-    No_ INT NOT NULL,
+CREATE TABLE Ticket (
+    TNo INT NOT NULL,
     MTitle VARCHAR(32) NOT NULL,
-    DateTime_ DATETIME NOT NULL,
+    ShowDateTime DATETIME NOT NULL,
     RNumber INT NOT NULL,
     TName VARCHAR(32) NOT NULL,
     Price INT NOT NULL,
-    Email VARCHAR(32) NOT NULL,
-    PRIMARY KEY(No_, MTitle, DateTime_, RNumber, TName),
-    FOREIGN KEY(MTitle, DateTime_, RNumber, TName) REFERENCES Showtime(MTitle, DateTime_, RNumber, TName)
+    Email VARCHAR(320) NOT NULL,
+    PRIMARY KEY(TNo, MTitle, DateTime_, RNumber, TName),
+    FOREIGN KEY(MTitle, ShowDateTime, RNumber, TName) REFERENCES Showtime(MTitle, ShowDateTime, RNumber, TName)
     ON UPDATE CASCADE ON DELETE CASCADE
 );
