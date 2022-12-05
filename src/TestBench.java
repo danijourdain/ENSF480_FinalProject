bad57ce33ce4c2e62d2d290b8dd4ff9a93c1e3cf
danijourdain <danijourdain@icloud.com>
2022-12-04 17:59:25 -0700

    fixed create statement

--- src/TestBench.java
+++ src/TestBench.java
@@ -5,16 +5,16 @@ import java.time.format.DateTimeFormatter;
 import java.util.Date;
 public class TestBench{
     public static void main(String args[]){
-        boolean caught = false;
-        User u = LoginRegisterManager.getInstance().login("carter.marcelo@ucalgary.ca","password");
-        System.out.println("Hello there");
+        // boolean caught = false;
+        // User u = LoginRegisterManager.getInstance().login("carter.marcelo@ucalgary.ca","password");
+        // System.out.println("Hello there");
     
-        // tesing purhcasing a ticket with credits
-        Movie movie = new Movie("Guardians of The Galaxy Vol.3", 122);
-        TheatreRoom tr = new TheatreRoom(250, 1);
-        Ticket t = new Ticket(1, 20, 1, LocalDateTime.parse("2022-12-20T16:30:00",DateTimeFormatter.ISO_DATE_TIME), movie, tr);
-        boolean b = FinanceManager.getInstance().applyCredit(t, u);
-        System.out.println(b);
+        // // tesing purhcasing a ticket with credits
+        // Movie movie = new Movie("Guardians of The Galaxy Vol.3", 122);
+        // TheatreRoom tr = new TheatreRoom(250, 1);
+        // Ticket t = new Ticket(1, 20, 1, LocalDateTime.parse("2022-12-20T16:30:00",DateTimeFormatter.ISO_DATE_TIME), movie, tr);
+        // boolean b = FinanceManager.getInstance().applyCredit(t, u);
+        // System.out.println(b);
     }
     
 }
