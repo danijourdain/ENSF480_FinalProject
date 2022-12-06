fc3beb9abeb3a785062cf3868b3abf30979fd46c
Nicholas Lam <100057820+Nicholas-Lam1@users.noreply.github.com>
2022-12-05 23:32:42 -0700

    sdfs

--- src/gui/CheckoutPage.java
+++ src/gui/CheckoutPage.java
@@ -162,7 +162,7 @@ public class CheckoutPage implements ActionListener {
     gbc.gridx = 1;
     gbc.gridy = 4;
     creditCard.setPreferredSize(new Dimension(50, 30));
-    if (user.getType().equals("Registered")) {
+    if(user.getType().equals("Registered")) {
       creditCard.setText(user.getCreditCard());
     }
     checkoutPage.add(creditCard, gbc);
